1a19b7d7ce204b6ed1bdbc8e1cbb72289b1ae452
nidawahid28 <wahidnida761@gmail.com>
2025-01-03 22:37:00 +0500

    updated

---
+++ src/app/components/Carousel.tsx
@@ -0,0 +1,54 @@
+import Image from 'next/image'
+import slide1 from '../images/slide1.jpg'
+import slide2 from '../images/slide2.jpg'
+import slide3 from '../images/slide3.jpg'
+import slide4 from '../images/slide4.jpg'
+
+export default function Carousel() {
+  return (
+    <>
+            <div className="carousel w-full">
+  <div id="slide1" className="carousel-item relative w-full">
+    <Image
+      src={slide1}
+      alt="image1"
+      className="w-full" />
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide4" className="btn btn-circle">❮</a>
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide2" className="carousel-item relative w-full">
+    <Image
+      src={slide2}
+      alt="image2"
+      className="w-full" />
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide1" className="btn btn-circle">❮</a>
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide3" className="carousel-item relative w-full">
+    <Image
+      src={slide3}
+      alt="image3"
+      className="w-full" />
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide2" className="btn btn-circle">❮</a>
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide4" className="carousel-item relative w-full">
+    <Image
+      src={slide4}
+      alt="image4"
+      className="w-full" />
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide3" className="btn btn-circle">❮</a>
+      <a href="#slide1" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
+    </>
+  )
+}
